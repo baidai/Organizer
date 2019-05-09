03213b1d543f609380842720516ca59eb4af793f
unknown <ml43@protonmail.com>
2019-05-09 11:48:08 +0800

    ADD messaging, Update Nav bar change after login, Update header

--- project/intro/views.py
+++ project/intro/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Task
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
-
+from django.contrib import messages
 
 # Create your views here.
 
@@ -16,11 +16,17 @@ def register(request):
 		form = UserCreationForm(request.POST)
 		if form.is_valid():
 			user = form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f"New Account Created:{username}")
 			login(request, user)
 			return redirect("intro:homepage")
 		else:
 			for msg in form.error_messages:
-				print(form.error_messages[msg])
+				messages.error(request, f"{msg}: {form.error_messages[msg]}")
+			
+			return render(request = request,
+				template_name = "intro/register.html",
+				context={"form":form})
 				
 	form = UserCreationForm
 	return render(request,
